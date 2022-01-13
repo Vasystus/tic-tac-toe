72fa2fb4af9feb709ec268e406a7f9763cd4497f
artgalla8 <artgalla8@gmail.com>
2022-01-13 18:57:13 +0000

    .html, .css and part of .js files added

---
+++ script.js
@@ -0,0 +1,49 @@
+
+const statusDisplay = document.querySelector('.game--status');
+/*
+ declare some variables. 
+*/
+/*
+We will use gameActive to pause the game in case of an end scenario
+*/
+let gameActive = true;
+/*
+Store our current player here
+*/
+let currentPlayer = "X";
+/*
+Store our current game state here, the form of empty strings in an array
+ will allow us to easily track played cells and validate the game state later on
+*/
+let gameState = ["", "", "", "", "", "", "", "", ""];
+/*
+Here we have declared some messages we will display to the user.
+*/
+const winningMessage = () => `Player ${currentPlayer} has won!`;
+const drawMessage = () => `Game ended in a draw!`;
+const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
+/*
+We set the inital message to let the players know whose turn it is
+*/
+statusDisplay.innerHTML = currentPlayerTurn();
+function handleCellPlayed() {
+
+}
+function handlePlayerChange() {
+
+}
+function handleResultValidation() {
+
+}
+function handleCellClick() {
+
+}
+function handleRestartGame() {
+
+}
+/*
+And finally we add our event listeners to the actual game cells, as well as our 
+restart button
+*/
+document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', handleCellClick));
+document.querySelector('.game--restart').addEventListener('click', handleRestartGame);
